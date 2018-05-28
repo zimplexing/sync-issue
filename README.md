152cc98384c6abb1d64b72390b3cded1111db9ec
张鑫14 <zhangxin14@hikvision.com>
2018-05-28 17:22:23 +0800

    add issue set command

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## sync-issue
 used to synchronize between local markdown file and github issues 
+
+
+1. 开发态软链接
+2. 本地全局安装
---
+++ libs/install.js
@@ -0,0 +1,18 @@
+const homedir = require('os').homedir()
+const path = require('path')
+const fs = require('fs')
+
+// TODO: 默认拉取`label`为`blog`的`issue`作为示例
+const initConfig = {
+  username: 'zimplexing',
+  repo: 'issue-sync',
+  pullType: 'all',
+  label: false
+}
+
+fs.writeFile(path.join(homedir, '.issue.json'), JSON.stringify(initConfig, null, 2), err => {
+  if (err) {
+    console.log(err);
+    return
+  }
+})
