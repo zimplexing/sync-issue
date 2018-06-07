ee48bdd450a4a5825630997feec7f6eb24202e28
张鑫14 <zhangxin14@hikvision.com>
2018-06-07 15:40:58 +0800

    【chg】add issue pull commander

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "standard"
+    "extends": "standard",
+    // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
 };
